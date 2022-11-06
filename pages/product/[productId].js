e51c8f086c48c9921cbb79a429e9bebf41b09abd
Sahil <leviack629@gmail.com>
2022-11-06 13:04:47 +0530

    restart

--- pages/product/[productId].js
+++ pages/product/[productId].js
@@ -1,10 +1,13 @@
+import { useRouter } from 'next/router'
 import React from 'react'
 
 export default function ProductDetails() {
+    const router = useRouter()
+    const productId = router.query.productId
   return (
     <div>
         <h1>
-        Details ABout The Product
+        Details About The Product {productId}
         </h1>
     </div>
   )
