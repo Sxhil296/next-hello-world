fabee05fc3e7539292c357fbc82966a58540645e
Sahil <leviack629@gmail.com>
2022-11-06 21:26:00 +0530

    Programmatically Navigation and 404 page

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,16 @@
 import Link from 'next/link'
+import { useRouter } from 'next/router'
 import React from 'react'
 
 const index = () => {
+
+  const router = useRouter()
+
+  const handleClick = () => {
+    console.log('Placing your order')
+    router.push('/product')
+  }
+  
   return (
     <div>
         <h1>Home Page</h1>
@@ -12,6 +21,7 @@ const index = () => {
         <Link href="/product">
           Product
         </Link>
+        <button onClick={handleClick}>Place Order</button>
     </div>
   )
 }
